0ddf1fb546fc95c9db3935824424a240c0d2a9a7
abhay0826R-dev <abhay0826r@gmail.com>
2026-06-16 20:30:16 +0530

    feat: /onboarding route

--- proxy.ts
+++ proxy.ts
@@ -1,21 +1,45 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from 'next/server'
+
+const isOnboardingRoute = createRouteMatcher(['/onboarding'])
 
 const isPublicRoute = createRouteMatcher([
     "/",
     "/sign-in(.*)",
     "/sign-up(.*)",
     "/api/links(.*)",
+    "/onboarding"
 ]);
 
-export default clerkMiddleware(async (auth, request) => {
-    if (!isPublicRoute(request)) {
-        await auth.protect();
+export default clerkMiddleware(async (auth, req: NextRequest) => {
+    const { isAuthenticated, sessionClaims, redirectToSignIn } = await auth()
+
+    // For users visiting /onboarding, don't try to redirec
+    if (isAuthenticated && isOnboardingRoute(req)) {
+        return NextResponse.next()
     }
+
+    // If the user isn't signed in and the route is private, redirect to sign-in
+    if (!isAuthenticated && !isPublicRoute(req)) return redirectToSignIn({ returnBackUrl: req.url })
+
+    // Catch users who do not have `onboardingComplete: true` in their publicMetadata
+    // Redirect them to the /onboarding route to complete onboarding
+    if (isAuthenticated && !sessionClaims?.metadata?.onboardingComplete) {
+        const onboardingUrl = new URL('/onboarding', req.url)
+        return NextResponse.redirect(onboardingUrl)
+    }
+
+    // If the user is signed in and the route is protected, let them view.
+    if (isAuthenticated && !isPublicRoute(req)) return NextResponse.next()
 });
 
 export const config = {
     matcher: [
+        // Skip Next.js internals and all static files, unless found in search params
         "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-        "/(api|trpc)(.*)",
+         // Always run for API routes
+        '/(api|trpc)(.*)',
+        // Always run for Clerk-specific frontend API routes
+        '/__clerk/(.*)',
     ],
 };
